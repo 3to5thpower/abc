9f01c51ff3a6c33677940183b513cbce20246643
3to5thpower <gen194265@gmail.com>
2019-03-10 00:36:15 +0900

    solve 121c

--- 121c_wa.cpp
+++ 121c_wa.cpp
@@ -13,30 +13,38 @@ using namespace std;
 #define all(x) (x).begin,(x).end()
 #define SORT(v, n) sort(v, v+n);
 #define VSORT(v) sort(v.begin(), v.end());
-#define vint vector<int>
-#define vvint vector<vector<int>>
 
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 int gcd(int a,int b){return b?gcd(b,a%b):a;}
 
 int n,m;
+vector<ll> a,b;
+ll ans=0;
 
-int main(){
-  cin >> n >> m; 
-  vector<pair<ll, ll>> ab(n);
-  rep(i,n)cin >> ab[i].first >> ab[i].second;
-  VSORT(ab);
-  
-  ll ans = 0;
-  int i=0;
-  while(m-ab[i].second>0){
-    ans+=ab[i].first*ab[i].second;
-    m-=ab[i].second;
-    i++;
+ll operate(int m){
+  ll mc;
+  //最小価格を探索
+  vector<ll>::iterator minIt = min_element(a.begin(),a.end());
+  size_t pos = distance(a.begin(), minIt);
+  mc=a[pos];
+  a[pos]=INF;
+
+  if(m-b[pos]>0){ 
+    ans += mc * b[pos];
+    return operate(m-b[pos]);
   }
-  ans +=ab[i].first*m;
+  else{
+    ans+= mc * m;
+    return ans;
+  }
+}
 
-  cout << ans << endl;
-  return 0;
+signed main(){
+  cin.tie(0);
+  cin >> n >> m;
+  a.resize(n);b.resize(n);
+  rep(i,n)cin >> a[i] >> b[i];
+  cout << operate(m) << endl;
+
 }
