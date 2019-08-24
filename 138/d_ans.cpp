99b1c0602be9c8e042e59931cd70c5a23a724134
3to5thpower <nisi3genki@gmail.com>
2019-08-24 20:39:48 +0900

    ABC138に参加

---
+++ 138/d_ans.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define INF 99999999
+#define rep(i,n) for(int i=0,temp=(int)(n);i<temp;++i) 
+#define repi(i,start,end) for(int i=start,temp=(int)(end);i<end;++i)
+#define rfor(v,x) for(const auto& x : v) //xは値を表す
+#define all(x) (x).begin(),(x).end()
+#define SORT(v, n) sort(v, v+n)
+#define vsort(v) sort(v.begin(), v.end())
+#define vfsort(v,lambda) sort(v.begin(),v.end(),lambda)
+#define vint vector<int>
+#define vvint vector<vector<int>>
+#define vin(v) rep(i,v.size()) {cin >> v[i];}
+//最終奥義
+//#define int long long
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+int ctoi(char c) {return c-'0';}
+//(lambda (x y) (< x y)) => [](int x, int y) -> int { return x<y; }
+
+signed main(void){
+  int n,q;cin >> n >> q;
+  vint super(n,-1);
+  rep(i,n-1){
+    int a,b;cin >> a >> b;
+    super[b-1]=a-1;
+  }
+  vint c(n,0);
+  rep(i,q){
+    int p,x;cin >> p >> x;
+    c[p-1]+=x;
+  }
+  rep(i,n-1)c[i+1]+=c[super[i+1]];
+  rep(i,n)
+    cout << c[i] << " ";
+  cout << endl;
+}
