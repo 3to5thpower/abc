9803694de41ad455418a9a5eb8fd2aaba85fd2a6
3to5thpower <nisi3genki@gmail.com>
2019-09-28 22:44:25 +0900

    ABC142に参加

---
+++ abc/142/d.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll =  long long;
+using pint =  pair<int,int>;
+
+#define INF 99999999
+#define rep(i,n) for(int (i)=0,temp=(int)(n);(i)<temp;++(i)) 
+#define repi(i,start,end) for(int (i)=(start),temp=(int)(end);i<(end);++(i))
+#define rfor(v,x) for(const auto& (x) : (v)) //xは値を表す
+#define all(x) (x).begin(),(x).end()
+#define SORT(v, n) sort((v), (v)+(n))
+#define vsort(v) sort((v).begin(), (v).end())
+#define vfsort(v,lambda) sort(all((v)),(lambda))
+#define vint vector<int>
+#define vvint vector<vector<int>> //vvint v(n,vint(n))のように宣言できる
+#define vin(v) rep(i,(v).size()) {cin >> (v)[i];}
+//最終奥義
+#define int long long
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+int ctoi(char c) {return c-'0';}
+//lambda式 -> [&](int x, int y){return x<y;}
+//vector内の重複を削除-> v.erase(unique(all(v)),v.end());
+
+signed main(void){
+  int a,b;cin >> a >> b;
+  if(b>a) swap(a,b);
+  int g=gcd(a,b);
+  int num=g;
+  int ans=1;
+  if(num%2==0){
+    ans++;
+    while(num%2==0)num/=2;
+  }
+  bool primeg=true;
+  for(int i=3;i*i<=g;i+=2){
+    if(num%i!=0)continue;
+    ans++;primeg=false;
+    while(num%i==0)num/=i;
+  }
+  if(g!=1&&primeg)ans++;
+
+  printf("%lld\n",ans);
+}
